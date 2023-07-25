e42f3a83e6580bfeeccc45da0fa74fa530eff6c9
sudharsanpv <sudharsanpv@gmail.com>
2023-07-25 11:04:53 +0530

    Updated key name

--- Terraform/VPC with Ans Jen Mav.tf
+++ Terraform/VPC with Ans Jen Mav.tf
@@ -5,11 +5,11 @@ provider "aws" {
 resource "aws_instance" "demo-server" {
     ami = "ami-053b0d53c279acc90"
     instance_type = "t2.micro"
-    key_name = "terraform key"
+    key_name = "prod"
     //security_groups = [ "demo-sg" ]
     vpc_security_group_ids = [aws_security_group.demo-sg.id]
     subnet_id = aws_subnet.dpp-public-subnet-01.id 
-for_each = toset(["jenkins-master", "build-slave", "ansible"])
+    for_each = toset(["jenkins-master", "build-slave", "ansible"])
    tags = {
      Name = "${each.key}"
    }
